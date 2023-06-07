72752eed7fa0bfa9c093ee286234b9ea31f63c1a
TatoZhvania <tatojvania.03@gmail.com>
2023-06-07 19:48:14 +0400

    first commit

---
+++ routes.php
@@ -0,0 +1,8 @@
+<?php
+
+$router->deifen([
+    '' => 'controllers/index.php',
+    'about' => 'controllers/about.php',
+    'about/culture' => 'controllers/about-culture.php',
+    'contact' => 'controllers/contact.php'
+]);
